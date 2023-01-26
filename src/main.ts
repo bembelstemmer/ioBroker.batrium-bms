47af178554acbd22a51c659019e252d65053bc8e
bembelstemmer <bembelstemmer@users.noreply.github.com>
2023-01-26 20:23:38 +0100

    First steps integrating binary parser and dgram socket

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,16 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 import * as utils from "@iobroker/adapter-core";
+import dgram from "node:dgram";
+import Parser from "binary-parser";
+
 
 // Load your modules here, e.g.:
 // import * as fs from "fs";
 
 class BatriumBms extends utils.Adapter {
+
+    private server: dgram.Socket;
 
     public constructor(options: Partial<utils.AdapterOptions> = {}) {
         super({
@@ -17,17 +22,24 @@ class BatriumBms extends utils.Adapter {
             name: "batrium-bms",
         });
         this.on("ready", this.onReady.bind(this));
-        this.on("stateChange", this.onStateChange.bind(this));
+        // this.on("stateChange", this.onStateChange.bind(this));
         // this.on("objectChange", this.onObjectChange.bind(this));
         // this.on("message", this.onMessage.bind(this));
         this.on("unload", this.onUnload.bind(this));
+
+        this.server = dgram.createSocket({ type: "udp4" });
     }
 
     /**
      * Is called when databases are connected and adapter received configuration.
      */
     private async onReady(): Promise<void> {
-        // Initialize your adapter here
+
+        this.server.on("error", this.onServerError.bind(this));
+        this.server.on("listening", this.onServerListening.bind(this));
+        this.server.on("message", this.onServerMessage.bind(this));
+        this.server.on("close", this.onServerClose.bind(this));
+        this.server.bind(18542);
 
         // Reset the connection indicator during startup
         this.setState("info.connection", false, true);
@@ -76,11 +88,11 @@ class BatriumBms extends utils.Adapter {
         await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
 
         // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync("admin", "iobroker");
-        this.log.info("check user admin pw iobroker: " + result);
+        //let result = await this.checkPasswordAsync("admin", "iobroker");
+        //this.log.info("check user admin pw iobroker: " + result);
 
-        result = await this.checkGroupAsync("admin", "admin");
-        this.log.info("check group user admin group admin: " + result);
+        //result = await this.checkGroupAsync("admin", "admin");
+        //this.log.info("check group user admin group admin: " + result);
     }
 
     /**
@@ -88,32 +100,12 @@ class BatriumBms extends utils.Adapter {
      */
     private onUnload(callback: () => void): void {
         try {
-            // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
-
+            this.server.close();
             callback();
         } catch (e) {
             callback();
         }
     }
-
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  */
-    // private onObjectChange(id: string, obj: ioBroker.Object | null | undefined): void {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
 
     /**
      * Is called if a subscribed state changes
@@ -128,22 +120,25 @@ class BatriumBms extends utils.Adapter {
         }
     }
 
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  */
-    // private onMessage(obj: ioBroker.Message): void {
-    //     if (typeof obj === "object" && obj.message) {
-    //         if (obj.command === "send") {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info("send command");
+    private onServerMessage(msg: Buffer, info: dgram.RemoteInfo): void {
+        this.log.info("MSG received");
+    }
 
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-    //         }
-    //     }
-    // }
+    private async onServerListening(): Promise<void> {
+        const address = this.server.address();
+        const port = address.port;
+        const ipaddr = address.address;
+        this.log.info(`UDP Listening started on ${ipaddr}:${port}`);
+    }
+
+    private async onServerClose(): Promise<void> {
+        this.log.info("UDP Listener Port closed.");
+    }
+
+    private onServerError(error: Error): void {
+        this.log.error("Error in listener: " + error.message);
+        this.restart();
+    }
 
 }
 
